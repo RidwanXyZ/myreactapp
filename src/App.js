dec2786aafe3c13d76d2eb49b56f9300bb027538
RidwanXyZ <ridwanwiki18220@gmail.com>
2023-06-12 12:48:05 +0800

    aaokaoa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Ridwan Wiki Saputro</Navbar.Brand>
+          <Nav className="justify-content-end" gap={3}>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About me</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container className='m-2'>
+      <Home className="text-center" />
+      </Container>
+    </>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,48 @@
+import Carousel from "react-bootstrap/Carousel";
+
+
+export default function Home() {
+  return (
+    <div id="home">
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 h-80"
+            src="/img/img1.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 h-80"
+            src="/img/img2.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 h-80"
+            src="/img/img3.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
